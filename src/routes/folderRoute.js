991d1e31cfa2be8880d489551138bc6a0212cf15
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-07-29 14:44:35 +0530

    added token verification for some of the apis

--- src/routes/folderRoute.js
+++ src/routes/folderRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { addFolder, deleteFolder } from "../controller/folderController";
 import validateFolderDeleteRequest from "../middleware/validateFolderDeleteRequest";
+import verifyToken from "../middleware/verifyToken";
 
 const router = express.Router();
 
-router.post("/add", addFolder);
-router.delete("/delete/:id", validateFolderDeleteRequest, deleteFolder);
+router.post("/add", verifyToken, addFolder);
+router.delete(
+  "/delete/:id",
+  verifyToken,
+  validateFolderDeleteRequest,
+  deleteFolder
+);
 
 export default router;
--- src/routes/formRoute.js
+++ src/routes/formRoute.js
@@ -8,14 +8,15 @@ import {
   updatedFormDetails,
 } from "../controller/formController.js";
 import validateFormResponse from "../middleware/validateFormResponse.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/add", addFormDetails);
-router.post("/update/:id", updatedFormDetails);
-router.delete("/delete/:id", deleteForm);
+router.post("/add", verifyToken, addFormDetails);
+router.post("/update/:id", verifyToken, updatedFormDetails);
+router.delete("/delete/:id", verifyToken, deleteForm);
 router.get("/userInput/:id", getFormToFill);
 router.post("/saveResponse", validateFormResponse, saveFormResponse);
-router.get("/response/:id", getUserResponse);
+router.get("/response/:id", verifyToken, getUserResponse);
 
 export default router;
